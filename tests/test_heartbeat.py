f45cd52ca48038bba7fa73c624f36bbbfd9f4351
Stephan Heuel <mail@ping13.net>
2021-11-14 15:53:26 +0100

    debugging and simplifying testing workflow and heartbeat test

--- tests/test_heartbeat.py
+++ tests/test_heartbeat.py
@@ -1,10 +1,15 @@
-from heospy import heos_player
+import heospy
 import logging
+import json
+
+logging.basicConfig(level=logging.DEBUG)
 
 def test_zero():
     try:
-        p = heos_player.HeosPlayer()
-    except heos_player.HeosPlayerConfigException:
+        p = heospy.HeosPlayer()
+        result = p.cmd("system/heart_beat", {})
+        assert(result.get("heos",{}).get("result") == "success")
+    except heospy.HeosPlayerConfigException:
         logging.warning("no config, but this is okay for now")
         pass
     assert True
